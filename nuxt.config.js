c85d5c538ecfb0d688f97b33d2dd4c8392bd203d
dedyksuntoro <39944254+dedyksuntoro@users.noreply.github.com>
2021-01-28 01:46:32 +0700

    Install & Templating

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,47 +1,63 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
   head: {
-    title: 'art_ekspedisi_admin',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+
+    //TAMBAHKAN CODE INI UNTUK MENAMBAHKAN EKSTERNAL JAVASCRIPT
+    script: [
+      { src: "/vendor/jquery/jquery.min.js" },
+      { src: "/vendor/bootstrap/js/bootstrap.bundle.min.js" },
+      { src: "/vendor/jquery-easing/jquery.easing.min.js" },
+      { src: "/js/sb-admin-2.min.js" }
     ]
   },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  //DAN LOAD FILE CSS DI DALAM ATTRIBUTE INI
   css: [
+    '@/assets/vendor/fontawesome-free/css/all.min.css',
+    '@/assets/css/sb-admin-2.min.css'
   ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  /*
+  ** Plugins to load before mounting the App
+  */
   plugins: [
   ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  /*
+  ** Nuxt.js dev-modules
+  */
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
+  /*
+  ** Nuxt.js modules
+  */
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  /*
+  ** Build configuration
+  */
   build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
   }
 }
